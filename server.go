9d333a3b8fb217bbfdc239eefe50b6e3f2461d69
erik <oomen.e@gmail.com>
2019-06-30 17:43:02 +0000

    Send data to kafka

--- server.go
+++ server.go
@@ -6,18 +6,31 @@ import (
     "fmt"
     "io/ioutil"
     "encoding/json"
+    "context"
+    kafka "github.com/segmentio/kafka-go"
 )
 
 const PORT = 3002
 
+func getKafkaWriter(kafkaURL, topic string) *kafka.Writer {
+	return kafka.NewWriter(kafka.WriterConfig{
+		Brokers:  []string{kafkaURL},
+		Topic:    topic,
+		Balancer: &kafka.LeastBytes{},
+	})
+}
+
 func main() {
+    kafkaWriter := getKafkaWriter("192.168.1.106:9092", "sensors")
+    defer kafkaWriter.Close()
+
     server, err := net.Listen("tcp", ":" + strconv.Itoa(PORT))
     if server == nil {
         panic(err)
     }
     conns := clientConns(server)
     for {
-        go handleConn(<-conns)
+        go handleConn(<-conns, kafkaWriter)
     }
 }
 
@@ -39,12 +52,18 @@ func clientConns(listener net.Listener) chan net.Conn {
     return ch
 }
 
-func handleConn(client net.Conn) {
-	data, err := ioutil.ReadAll(client)
+func handleConn(client net.Conn, kafkaWriter *kafka.Writer) {
+	data,_ := ioutil.ReadAll(client)
 	client.Close()
 	var result map[string]interface{}
 	json.Unmarshal([]byte(data),  &result)
-	fmt.Printf("data %s %s", data, result)
+	fmt.Printf("data %s", data)
+	fmt.Printf("Content: ", result["version"], "button: ", result["button"])
+	msg := kafka.Message{
+			Key:   []byte(fmt.Sprintf("address-%s", result["type"])),
+			Value: data,
+			}
+	err := kafkaWriter.WriteMessages(context.Background(), msg)
 	fmt.Printf("error %s", err)
 }
 
